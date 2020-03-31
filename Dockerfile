4078278deaa8ba21bc00407f18394ed5ea5b3489
David Moreau <dmoreau@lamobilery.fr>
2020-03-31 18:40:02 +0200

    Serve with minimal Python server

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM mongo:4
 WORKDIR /app
 # Install gcloud SDK
 RUN apt update && \
-    apt install -y curl python && \
+    apt install -y curl python3 && \
     rm -fr /var/lib/apt/lists/*
 RUN curl https://sdk.cloud.google.com > install.sh && \
     bash install.sh --disable-prompts && \
     rm install.sh
 ENV PATH="/root/google-cloud-sdk/bin:${PATH}"
-# Copy and launch backup script
+# Copy scripts and launch Python server
 COPY mongo-backup.sh .
-ENTRYPOINT ["/bin/sh", "mongo-backup.sh"]
+RUN chmod +x mongo-backup.sh
+COPY server.py .
+ENTRYPOINT ["/usr/bin/python3", "server.py"]
---
+++ server.py
@@ -0,0 +1,29 @@
+from subprocess import check_output, CalledProcessError
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+port = 8080
+script_path = './mongo-backup.sh' 
+
+class BashScriptHTTPRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    response = b''
+    try:
+      if self.path != '/':
+        self.send_response(404)
+      else:
+        response = check_output(script_path)
+        self.send_response(200)
+    except CalledProcessError as error:
+      response = b"-----------------------------\n"
+      response += b"Error during script execution\n"
+      response += b"-----------------------------\n\n"
+      response += error.output
+      self.send_response(500)
+    finally:
+      self.send_header("Content-type", "text/plain")
+      self.end_headers()
+      self.wfile.write(response)
+
+httpd = HTTPServer(('0.0.0.0', port), BashScriptHTTPRequestHandler)
+print("Serving at port", port)
+httpd.serve_forever()
